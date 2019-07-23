fe500403ddeec098f53ee1798a38b6bdd7aedaa7
oussamabgz <oussamamage16@hotmail.com>
2019-07-23 17:25:53 +0100

    add db connection

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
+const connectDB = require('./config/db')
+
+// Connect Database
+connectDB()
+
 
 app.get('/', (req, res) => res.send('API Running'))
 
